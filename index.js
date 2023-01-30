053ff20894c5c8ea6a989292478073f5979b4d60
bin4black <122944796+bin4black@users.noreply.github.com>
2023-01-30 07:58:47 +0100

    1

--- index.js
+++ index.js
@@ -5,4 +5,27 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-kkkk
+let stocks = {
+    Fruits: ['strawberry', 'grapes', 'banana', 'apple'],
+    liquid: ['water', 'ice'],
+    holder: ['cone', 'cup', 'stick'],
+    toppings: ['chocolate', 'peanuts'],
+  };
+  let is_shop_open = true;
+
+  function time(ms) {
+    return new Promise( (resolve, reject) =>{
+        if (is_shop_open){
+            setTimeout(resolve, ms);
+        } else {
+            reject (console.log("Shop is Closed - inside Promise"))
+        }
+   })
+  }
+
+  async function kitchen(){
+    try{}
+    catch(error){}
+    finally{}
+  }
+kitchen()
